596155beaeaffa1816eae4b7b7533c782b70c6cd
hebangwen <1925585604@qq.com>
2025-03-26 09:00:21 +0800

    [WIP] add sigmoid

--- kernels/sigmoid/sigmoid.ref.cu
+++ kernels/sigmoid/sigmoid.ref.cu
@@ -26,81 +26,111 @@
 // Sigmoid x: N, y: N y=1/(1+exp(-x))
 // grid(N/256), block(K=256) 
 __global__ void sigmoid_f32_kernel(float* x, float* y, int N) {
-    // [START MANUAL IMPLEMENTATION]
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (tid < N) {
-      // 注意这后面添加的后缀 f，表明这些函数针对的是 float 类型，如果没有 f，则是 double 类型
-      float val = fmaxf(fminf(x[tid], MAX_EXP_F32), MIN_EXP_F32);
-      float sig = 1 / (1 + expf(-val));
-      y[tid] = sig;
-    }
-    // [END MANUAL IMPLEMENTATION]
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx < N) {
+    float v = x[idx];
+    v = fminf(fmaxf(v, MIN_EXP_F32), MAX_EXP_F32);
+    y[idx] = 1.0f / (1.0f + expf(-v));
+  }
 }
 
 // Sigmoid x: N, y: N y=1/(1+exp(-x)) Vec4
 // grid(N/256), block(256/4)
 __global__ void sigmoid_f32x4_kernel(float* x, float* y, int N) {
-    // [START MANUAL IMPLEMENTATION]
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    int gid = tid << 2;
-    if (gid < N) {
-      float reg_x[4], reg_sig[4];
-      LDST128BITS(reg_x[0]) = LDST128BITS(x[gid]);
-      #pragma unroll
-      for (int i = 0; i < 4; i++) {
-        float clip = fmaxf(fminf(reg_x[i], MAX_EXP_F32), MIN_EXP_F32);
-        reg_sig[i] = 1 / (1 + expf(-clip));
-      }
+  int idx = (blockIdx.x * blockDim.x + threadIdx.x) * 4;
+  float4 reg_x = FLOAT4(x[idx]);
+  float4 reg_y;
+    
+  reg_x.x = fminf(fmaxf(reg_x.x, MIN_EXP_F32), MAX_EXP_F32);
+  reg_x.y = fminf(fmaxf(reg_x.y, MIN_EXP_F32), MAX_EXP_F32);
+  reg_x.z = fminf(fmaxf(reg_x.z, MIN_EXP_F32), MAX_EXP_F32);
+  reg_x.w = fminf(fmaxf(reg_x.w, MIN_EXP_F32), MAX_EXP_F32);
 
-      LDST128BITS(y[gid]) = LDST128BITS(reg_sig[0]);
-    }
-    // [END MANUAL IMPLEMENTATION]
+  reg_y.x = 1.0f / (1.0f + expf(-reg_x.x));
+  reg_y.y = 1.0f / (1.0f + expf(-reg_x.y));
+  reg_y.z = 1.0f / (1.0f + expf(-reg_x.z));
+  reg_y.w = 1.0f / (1.0f + expf(-reg_x.w));
+
+  if ((idx + 0) < N) { FLOAT4(y[idx]) = reg_y; }
 }
 
 // -------------------------------------- FP16 -------------------------------------- 
 __global__ void sigmoid_f16_kernel(half* x, half* y, int N) {
-    // [START MANUAL IMPLEMENTATION]
-    // 使用 hexp 和 __hmax __hmin , operator< > 也可以用
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (tid < N) {
-      half val = __hmax(__hmin(x[tid], MAX_EXP_F16), MIN_EXP_F16);
-      // 注意：这条语句编译会产生报错，不支持 int + __half 的操作符
-      // half sig = 1 / (1 + hexp(-val));
-      // y[tid] = sig;
-    }
-    // [END MANUAL IMPLEMENTATION]
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  const half f = __float2half(1.0f);
+  if (idx < N) {
+    half v = x[idx];
+    v = __hmin(__hmax(v, MIN_EXP_F16), MAX_EXP_F16);
+    y[idx] = f / (f + hexp(-v));
+  }
 }
 
 __global__ void sigmoid_f16x2_kernel(half* x, half* y, int N) {
-    // [START MANUAL IMPLEMENTATION]
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    int gid = tid << 1;
-    if (gid < N) {
-      half2 reg_x = HALF2(x[gid]);
-      reg_x.x = __hmax(__hmin(reg_x.x, MAX_EXP_F16), MIN_EXP_F16);
-      reg_x.y = __hmax(__hmin(reg_x.y, MAX_EXP_F16), MIN_EXP_F16);
+  int idx = (blockIdx.x * blockDim.x + threadIdx.x) * 2;
+  const half f = __float2half(1.0f);
+  half2 reg_x = HALF2(x[idx]);
+  half2 reg_y;
+  reg_x.x = __hmin(__hmax(reg_x.x, MIN_EXP_F16), MAX_EXP_F16);
+  reg_x.y = __hmin(__hmax(reg_x.y, MIN_EXP_F16), MAX_EXP_F16);
 
-      half2 reg_sig;
-      reg_sig.x = 1 / (1 + hexp(-reg_x.x));
-      reg_sig.y = 1 / (1 + hexp(-reg_x.y));
+  reg_y.x = f / (f + hexp(-reg_x.x));
+  reg_y.y = f / (f + hexp(-reg_x.y));
 
-      HALF2(y[gid]) = reg_sig;
-    }
-    // [END MANUAL IMPLEMENTATION]
+  if ((idx + 0) < N) { HALF2(y[idx]) = reg_y; }
 }
 
 // unpack f16x8
 __global__ void sigmoid_f16x8_kernel(half* x, half* y, int N) {
-    // [START MANUAL IMPLEMENTATION]
-    // TODO: 请在此实现内核代码
-    // [END MANUAL IMPLEMENTATION]
+  int idx = (blockIdx.x * blockDim.x + threadIdx.x) * 8;
+  const half f = __float2half(1.0f);
+
+  half2 reg_x_0 = HALF2(x[idx + 0]);
+  half2 reg_x_1 = HALF2(x[idx + 2]);
+  half2 reg_x_2 = HALF2(x[idx + 4]);
+  half2 reg_x_3 = HALF2(x[idx + 6]);
+
+  reg_x_0.x = __hmin(__hmax(reg_x_0.x, MIN_EXP_F16), MAX_EXP_F16);
+  reg_x_0.y = __hmin(__hmax(reg_x_0.y, MIN_EXP_F16), MAX_EXP_F16);
+  reg_x_1.x = __hmin(__hmax(reg_x_1.x, MIN_EXP_F16), MAX_EXP_F16);
+  reg_x_1.y = __hmin(__hmax(reg_x_1.y, MIN_EXP_F16), MAX_EXP_F16);
+  reg_x_2.x = __hmin(__hmax(reg_x_2.x, MIN_EXP_F16), MAX_EXP_F16);
+  reg_x_2.y = __hmin(__hmax(reg_x_2.y, MIN_EXP_F16), MAX_EXP_F16);
+  reg_x_3.x = __hmin(__hmax(reg_x_3.x, MIN_EXP_F16), MAX_EXP_F16);
+  reg_x_3.y = __hmin(__hmax(reg_x_3.y, MIN_EXP_F16), MAX_EXP_F16);
+
+  half2 reg_y_0, reg_y_1, reg_y_2, reg_y_3;
+  
+  reg_y_0.x = f / (f + hexp(-reg_x_0.x));
+  reg_y_0.y = f / (f + hexp(-reg_x_0.y));
+  reg_y_1.x = f / (f + hexp(-reg_x_1.x));
+  reg_y_1.y = f / (f + hexp(-reg_x_1.y));
+  reg_y_2.x = f / (f + hexp(-reg_x_2.x));
+  reg_y_2.y = f / (f + hexp(-reg_x_2.y));
+  reg_y_3.x = f / (f + hexp(-reg_x_3.x));
+  reg_y_3.y = f / (f + hexp(-reg_x_3.y));
+
+  if ((idx + 0) < N) { HALF2(y[idx + 0]) = reg_y_0; }
+  if ((idx + 2) < N) { HALF2(y[idx + 2]) = reg_y_1; }
+  if ((idx + 4) < N) { HALF2(y[idx + 4]) = reg_y_2; }
+  if ((idx + 6) < N) { HALF2(y[idx + 6]) = reg_y_3; }
 }
 
 // pack f16x8
 __global__ void sigmoid_f16x8_pack_kernel(half* x, half* y, int N) {
-    // [START MANUAL IMPLEMENTATION]
-    // TODO: 请在此实现内核代码
-    // [END MANUAL IMPLEMENTATION]
+  int idx = (blockIdx.x * blockDim.x + threadIdx.x) * 8;
+  const half f = __float2half(1.0f);
+  // temporary register(memory), .local space in ptx, addressable
+  half pack_x[8], pack_y[8]; // 8x16 bits=128 bits.
+  // reinterpret as float4 and load 128 bits in 1 memory issue.
+  LDST128BITS(pack_x[0]) = LDST128BITS(x[idx]); // load 128 bits
+  
+  #pragma unroll
+  for (int i = 0; i < 8; ++i) {
+    half v = __hmin(__hmax(pack_x[i], MIN_EXP_F16), MAX_EXP_F16);
+    pack_y[i] = f / (f + hexp(-v));
+  }
+  // reinterpret as float4 and store 128 bits in 1 memory issue.
+  if ((idx + 7) < N) { LDST128BITS(y[idx]) = LDST128BITS(pack_y[0]); }
 }
 
 // --------------------- PyTorch bindings for custom kernel -----------------------
