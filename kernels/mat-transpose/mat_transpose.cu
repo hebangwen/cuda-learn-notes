8d3cd2b4bd38c7ccfb12e1dbbaa9885a2dc1719c
hebangwen <1925585604@qq.com>
2025-03-28 09:00:50 +0800

    [WIP] introduce mat-transpose and fix grid error

--- kernels/mat-transpose/mat_transpose.cu
+++ kernels/mat-transpose/mat_transpose.cu
@@ -28,250 +28,164 @@
 // row2col means read x[col][row] and write y[row][col]
 __global__ void mat_transpose_f32_col2row_kernel(
   float *x, float *y, const int row, const int col) {
-  const int global_idx = blockIdx.x * blockDim.x + threadIdx.x;
-  const int global_row = global_idx / col;
-  const int global_col = global_idx % col;
-  if (global_idx < row * col) {
-    y[global_col * row + global_row] = x[global_idx];
-  }
+    // [START MANUAL IMPLEMENTATION]
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int N = row * col;
+    if (tid < N) {
+      int yrow = tid % col;
+      int ycol = tid / col;
+      y[yrow * row + ycol] = x[tid];
+    }
+    // [END MANUAL IMPLEMENTATION]
 }
 
 __global__ void mat_transpose_f32_row2col_kernel(
   float *x, float *y, const int row, const int col) {
-  const int global_idx = blockIdx.x * blockDim.x + threadIdx.x;
-  const int global_col = global_idx / row;
-  const int global_row = global_idx % row;
-  if (global_idx < row * col) {
-    y[global_idx] = x[global_row * col + global_col];
-  }
+    // [START MANUAL IMPLEMENTATION]
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int N = row * col;
+    if (tid < N) {
+      int xrow = tid % row;
+      int xcol = tid / row;
+      y[tid] = x[xrow * col + xcol];
+    }
+    // [END MANUAL IMPLEMENTATION]
 }
 
 __global__ void mat_transpose_f32x4_col2row_kernel(
   float *x, float *y, const int row, const int col) {
-  int global_idx = blockIdx.x * blockDim.x + threadIdx.x;
-  int global_col = (global_idx * 4) % col;
-  int global_row = (global_idx * 4) / col;
-
-  if (global_row < row && global_col + 3 < col) {
-    float4 x_val = reinterpret_cast<float4 *>(x)[global_idx];
-
-    y[global_col * row + global_row] = x_val.x;
-    y[(global_col + 1) * row + global_row] = x_val.y;
-    y[(global_col + 2) * row + global_row] = x_val.z;
-    y[(global_col + 3) * row + global_row] = x_val.w;
-  }
+    // [START MANUAL IMPLEMENTATION]
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int gid = tid << 2;
+    int N = row * col;
+    if (gid < N) {
+      float4 rx = FLOAT4(x[gid]);
+      int yrow = gid % col;
+      int ycol = gid / col;
+      y[yrow * row + ycol] = rx.x;
+      y[yrow * row + ycol + row] = rx.y;
+      y[yrow * row + ycol + row * 2] = rx.z;
+      y[yrow * row + ycol + row * 3] = rx.w;
+    }
+    // [END MANUAL IMPLEMENTATION]
 }
+
 __global__ void mat_transpose_f32x4_row2col_kernel(
   float *x, float *y, const int row, const int col) {
-  const int global_idx = blockIdx.x * blockDim.x + threadIdx.x;
-  const int global_col = (global_idx * 4) / row;
-  const int global_row = (global_idx * 4) % row;
-
-  if (global_row < row && global_col < col) {
-    float4 x_val;
-    x_val.x = x[global_row * col + global_col];
-    x_val.y = x[(global_row + 1) * col + global_col];
-    x_val.z = x[(global_row + 2) * col + global_col];
-    x_val.w = x[(global_row + 3) * col + global_col];
-    reinterpret_cast<float4 *>(y)[global_idx] = FLOAT4(x_val);
-  }
+    // [START MANUAL IMPLEMENTATION]
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int gid = tid << 2;
+    int N = row * col;
+    if (gid < N) {
+      float4 ry;
+      int xrow = gid % row;
+      int xcol = gid / row;
+      ry.x = x[xrow * col + xcol];
+      ry.y = x[xrow * col + xcol + col];
+      ry.z = x[xrow * col + xcol + col * 2];
+      ry.w = x[xrow * col + xcol + col * 3];
+      FLOAT4(y[gid]) = ry;
+    }
+    // [END MANUAL IMPLEMENTATION]
 }
 
 // work for row == col
 __global__ void mat_transpose_f32_diagonal2d_kernel(
   float *x, float *y, int row, int col) {
-  const int block_y = blockIdx.x;
-  const int block_x = (blockIdx.x + blockIdx.y) % gridDim.x;
-  const int global_col = threadIdx.x + blockDim.x * block_x;
-  const int global_row = threadIdx.y + blockDim.y * block_y;
-  if (global_col < col && global_row < row) {
-    y[global_row * col + global_col] = x[global_col * row + global_row];
-  }
+    // [START MANUAL IMPLEMENTATION]
+    int xcol = threadIdx.x + blockIdx.x * blockDim.x;
+    int xrow = threadIdx.y + blockIdx.y * blockDim.y;
+    if (xcol < col && xrow < row) {
+      y[xcol * row + xrow] = x[xrow * col + xcol];
+    }
+    // [END MANUAL IMPLEMENTATION]
 }
 
 __global__ void mat_transpose_f32_col2row2d_kernel(
   float *x, float *y, const int row, const int col) {
-  const int global_x = blockIdx.x * blockDim.x + threadIdx.x;
-  const int global_y = blockIdx.y * blockDim.y + threadIdx.y;
-  if (global_x < col && global_y < row) {
-    y[global_x * row + global_y] = x[global_y * col + global_x];
-  }
+    // [START MANUAL IMPLEMENTATION]
+    int xcol = threadIdx.x + blockIdx.x * blockDim.x;
+    int xrow = threadIdx.y + blockIdx.y * blockDim.y;
+    if (xcol < col && xrow < row) {
+      y[xcol * row + xrow] = x[xrow * col + xcol];
+    }
+    // [END MANUAL IMPLEMENTATION]
 }
 
 __global__ void mat_transpose_f32_row2col2d_kernel(
   float *x, float *y, const int row, const int col) {
-  const int global_y = blockIdx.x * blockDim.x + threadIdx.x;
-  const int global_x = blockIdx.y * blockDim.y + threadIdx.y;
-  if (global_y < col && global_x < row) {
-    y[global_y * row + global_x] = x[global_x * col + global_y];
-  }
+    // [START MANUAL IMPLEMENTATION]
+    int yrow = threadIdx.x + blockIdx.x * blockDim.x;
+    int ycol = threadIdx.y + blockIdx.y * blockDim.y;
+    if (ycol < row && yrow < col) {
+      y[yrow * row + ycol] = x[ycol * col + yrow];
+    }
+    // [END MANUAL IMPLEMENTATION]
 }
 
 __global__ void mat_transpose_f32x4_col2row2d_kernel(
   float *x, float *y, const int row, const int col) {
-  const int global_x = blockIdx.x * blockDim.x + threadIdx.x;
-  const int global_y = blockIdx.y * blockDim.y + threadIdx.y;
-  if (global_x * 4 + 3 < col && global_y < row) {
-    float4 x_val = reinterpret_cast<float4 *>(x)[global_y * col / 4 + global_x];
-    y[(global_x * 4) * row + global_y] = x_val.x;
-    y[(global_x * 4 + 1) * row + global_y] = x_val.y;
-    y[(global_x * 4 + 2) * row + global_y] = x_val.z;
-    y[(global_x * 4 + 3) * row + global_y] = x_val.w;
-  }
+    // [START MANUAL IMPLEMENTATION]
+    int xcol = (threadIdx.x + blockIdx.x * blockDim.x) << 2;
+    int xrow = threadIdx.y + blockIdx.y * blockDim.y;
+    if (xcol < col && xrow < row) {
+      float4 rx = FLOAT4(x[xrow * col + xcol]);
+      y[xcol * row + xrow] = rx.x;
+      y[(xcol + 1) * row + xrow] = rx.y;
+      y[(xcol + 2) * row + xrow] = rx.z;
+      y[(xcol + 3) * row + xrow] = rx.w;
+    }
+    // [END MANUAL IMPLEMENTATION]
 }
+
 __global__ void mat_transpose_f32x4_row2col2d_kernel(
   float *x, float *y, const int row, const int col) {
-  const int global_x = blockIdx.x * blockDim.x + threadIdx.x;
-  const int global_y = blockIdx.y * blockDim.y + threadIdx.y;
-  if (global_y * 4 + 3 < row && global_x < col) {
-    float4 x_val;
-    x_val.x = x[(global_y * 4) * col + global_x];
-    x_val.y = x[(global_y * 4 + 1) * col + global_x];
-    x_val.z = x[(global_y * 4 + 2) * col + global_x];
-    x_val.w = x[(global_y * 4 + 3) * col + global_x];
-    reinterpret_cast<float4 *>(y)[global_x * row / 4 + global_y] = FLOAT4(x_val);
-  }
+    // [START MANUAL IMPLEMENTATION]
+    int yrow = threadIdx.x + blockIdx.x * blockDim.x;
+    int ycol = (threadIdx.y + blockIdx.y * blockDim.y) << 2;
+    if (ycol < row && yrow < col) {
+      float4 ry;
+      ry.x = x[ycol * col + yrow];
+      ry.y = x[(ycol + 1) * col + yrow];
+      ry.z = x[(ycol + 2) * col + yrow];
+      ry.w = x[(ycol + 3) * col + yrow];
+      FLOAT4(y[yrow * row + ycol]) = ry;
+    }
+    // [END MANUAL IMPLEMENTATION]
 }
 
 
 __global__ void mat_transpose_f32x4_shared_col2row2d_kernel(
   float *x, float *y, const int row, const int col){
-  const int global_x = blockIdx.x * blockDim.x + threadIdx.x;
-  const int global_y = blockIdx.y * blockDim.y + threadIdx.y;
-  const int local_x = threadIdx.x;
-  const int local_y = threadIdx.y;
-  __shared__ float tile[WARP_SIZE_S][WARP_SIZE_S * 4];
-  if(global_x * 4 + 3 < col + 3 && global_y < row) {
-    // load value from x to shared memory
-    float4 x_val = reinterpret_cast<float4*>(x)[global_y * col / 4 + global_x];
-    FLOAT4(tile[local_y][local_x * 4]) = FLOAT4(x_val);
-    __syncthreads();
-    float4 smem_val;
-    // load value from shared memory to y.
-    // add STRIDE to satisfied different block size.
-    constexpr int STRIDE = WARP_SIZE_S / 4;
-    smem_val.x = tile[(local_y % STRIDE) * 4    ][local_x * 4 + local_y / STRIDE];
-    smem_val.y = tile[(local_y % STRIDE) * 4 + 1][local_x * 4 + local_y / STRIDE];
-    smem_val.z = tile[(local_y % STRIDE) * 4 + 2][local_x * 4 + local_y / STRIDE];
-    smem_val.w = tile[(local_y % STRIDE) * 4 + 3][local_x * 4 + local_y / STRIDE];
-    //map index n*n to (n/4)*(n*4)
-    const int bid_y = blockIdx.y * blockDim.y;
-    const int out_y = global_x * 4 + local_y / STRIDE;
-    const int out_x = (local_y % STRIDE) * 4 + bid_y;
-    reinterpret_cast<float4*>(y)[(out_y * row + out_x) / 4] = FLOAT4(smem_val);
-  }
+    // [START MANUAL IMPLEMENTATION]
+    // 每个子线程读取 4 个 float 到 shared memory 中
+    // 等 block 内读取完毕之后，同步
+    // 从共享内存中取出被转置的 4 个 float
+    // 写回到 y 中
+    // [END MANUAL IMPLEMENTATION]
 }
 
 __global__ void mat_transpose_f32x4_shared_row2col2d_kernel(
   float *x, float *y, const int row, const int col){
-  const int global_x = blockIdx.x * blockDim.x + threadIdx.x;
-  const int global_y = blockIdx.y * blockDim.y + threadIdx.y;
-  const int local_x = threadIdx.x;
-  const int local_y = threadIdx.y;
-  __shared__ float tile[WARP_SIZE_S * 4][WARP_SIZE_S];
-  if(global_y * 4 < row && global_x < col) {
-    // load value from x to shared memory
-    float4 x_val;
-    x_val.x = x[(global_y * 4) * col + global_x];
-    x_val.y = x[(global_y * 4 + 1) * col + global_x];
-    x_val.z = x[(global_y * 4 + 2) * col + global_x];
-    x_val.w = x[(global_y * 4 + 3) * col + global_x];
-    tile[local_y * 4    ][local_x] = x_val.x;
-    tile[local_y * 4 + 1][local_x] = x_val.y;
-    tile[local_y * 4 + 2][local_x] = x_val.z;
-    tile[local_y * 4 + 3][local_x] = x_val.w;
-    __syncthreads();
-    float4 smem_val;
-    // load value from shared memory to y.
-    // add STRIDE to satisfied different block size.
-    //map index n*n to (n/4)*(n*4)
-    constexpr int STRIDE = WARP_SIZE_S / 4;
-    smem_val.x = tile[local_x * 4 + local_y / STRIDE][(local_y % STRIDE) * 4];
-    smem_val.y = tile[local_x * 4 + local_y / STRIDE][(local_y % STRIDE) * 4 + 1];
-    smem_val.z = tile[local_x * 4 + local_y / STRIDE][(local_y % STRIDE) * 4 + 2];
-    smem_val.w = tile[local_x * 4 + local_y / STRIDE][(local_y % STRIDE) * 4 + 3];
-    const int bid_x = blockIdx.x * blockDim.x;
-    const int bid_y = blockIdx.y * blockDim.y;
-
-    const int out_y = bid_x + (local_y % STRIDE) * 4;
-    const int out_x = bid_y * 4 + local_x * 4 + (local_y / STRIDE);
-    y[out_y * row + out_x] = smem_val.x;
-    y[(out_y + 1) * row + out_x] = smem_val.y;
-    y[(out_y + 2) * row + out_x] = smem_val.z;
-    y[(out_y + 3) * row + out_x] = smem_val.w;
-  }
+    // [START MANUAL IMPLEMENTATION]
+    // TODO: 请在此实现内核代码
+    // [END MANUAL IMPLEMENTATION]
 }
 
 __global__ void mat_transpose_f32x4_shared_bcf_col2row2d_kernel(
   float *x, float *y, const int row, const int col){
-    const int global_x = blockIdx.x * blockDim.x + threadIdx.x;
-    const int global_y = blockIdx.y * blockDim.y + threadIdx.y;
-    const int local_x = threadIdx.x;
-    const int local_y = threadIdx.y;
-    __shared__ float tile[WARP_SIZE_S][WARP_SIZE_S * 4 + PAD];
-    if(global_x * 4 + 3 < col + 3 && global_y < row) {
-      // load value from x to shared memory
-      float4 x_val = reinterpret_cast<float4*>(x)[global_y * col / 4 + global_x];
-      tile[local_y][local_x * 4    ] = x_val.x;
-      tile[local_y][local_x * 4 + 1] = x_val.y;
-      tile[local_y][local_x * 4 + 2] = x_val.z;
-      tile[local_y][local_x * 4 + 3] = x_val.w;
-      __syncthreads();
-      float4 smem_val;
-      // load value from shared memory to y.
-      // add STRIDE to satisfied different block size.
-      constexpr int STRIDE = WARP_SIZE_S / 4;
-      smem_val.x = tile[(local_y % STRIDE) * 4    ][local_x * 4 + local_y / STRIDE];
-      smem_val.y = tile[(local_y % STRIDE) * 4 + 1][local_x * 4 + local_y / STRIDE];
-      smem_val.z = tile[(local_y % STRIDE) * 4 + 2][local_x * 4 + local_y / STRIDE];
-      smem_val.w = tile[(local_y % STRIDE) * 4 + 3][local_x * 4 + local_y / STRIDE];
-      //map index n*n to (n/4)*(n*4)
-      const int bid_y = blockIdx.y * blockDim.y;
-      const int out_y = global_x * 4 + local_y / STRIDE;
-      const int out_x = (local_y % STRIDE) * 4 + bid_y;
-      reinterpret_cast<float4*>(y)[(out_y * row + out_x) / 4] = FLOAT4(smem_val);
-    }
+    // [START MANUAL IMPLEMENTATION]
+    // TODO: 请在此实现内核代码
+    // [END MANUAL IMPLEMENTATION]
 }
 
 __global__ void mat_transpose_f32x4_shared_bcf_row2col2d_kernel(
   float *x, float *y, const int row, const int col){
-  const int global_x = blockIdx.x * blockDim.x + threadIdx.x;
-  const int global_y = blockIdx.y * blockDim.y + threadIdx.y;
-  const int local_x = threadIdx.x;
-  const int local_y = threadIdx.y;
-  __shared__ float tile[WARP_SIZE_S * 4][WARP_SIZE_S + PAD];
-  if(global_y * 4 < row && global_x < col) {
-    // load value from x to shared memory
-    float4 x_val;
-    x_val.x = x[(global_y * 4) * col + global_x];
-    x_val.y = x[(global_y * 4 + 1) * col + global_x];
-    x_val.z = x[(global_y * 4 + 2) * col + global_x];
-    x_val.w = x[(global_y * 4 + 3) * col + global_x];
-    tile[local_y * 4    ][local_x] = x_val.x;
-    tile[local_y * 4 + 1][local_x] = x_val.y;
-    tile[local_y * 4 + 2][local_x] = x_val.z;
-    tile[local_y * 4 + 3][local_x] = x_val.w;
-    __syncthreads();
-    float4 smem_val;
-    // load value from shared memory to y.
-    // add STRIDE to satisfied different block size.
-    //map index n*n to (n/4)*(n*4)
-    constexpr int STRIDE = WARP_SIZE_S / 4;
-    smem_val.x = tile[local_x * 4 + local_y / STRIDE][(local_y % STRIDE) * 4];
-    smem_val.y = tile[local_x * 4 + local_y / STRIDE][(local_y % STRIDE) * 4 + 1];
-    smem_val.z = tile[local_x * 4 + local_y / STRIDE][(local_y % STRIDE) * 4 + 2];
-    smem_val.w = tile[local_x * 4 + local_y / STRIDE][(local_y % STRIDE) * 4 + 3];
-    const int bid_x = blockIdx.x * blockDim.x;
-    const int bid_y = blockIdx.y * blockDim.y;
-
-    const int out_y = bid_x + (local_y % STRIDE) * 4;
-    const int out_x = bid_y * 4 + local_x * 4 + (local_y / STRIDE);
-    y[out_y * row + out_x] = smem_val.x;
-    y[(out_y + 1) * row + out_x] = smem_val.y;
-    y[(out_y + 2) * row + out_x] = smem_val.z;
-    y[(out_y + 3) * row + out_x] = smem_val.w;
-  }
+    // [START MANUAL IMPLEMENTATION]
+    // TODO: 请在此实现内核代码
+    // [END MANUAL IMPLEMENTATION]
 }
+
 // TODO: may support double buffer pipeline mat transpose ?
 // TODO: may support fp16 mat transpose ?
 
@@ -310,7 +224,7 @@ __global__ void mat_transpose_f32x4_shared_bcf_row2col2d_kernel(
     const int N = x.size(1);                                                                    \
     dim3 block(WARP_SIZE_S, WARP_SIZE_S);                                                       \
     dim3 grid((N + WARP_SIZE_S - 1) / (WARP_SIZE_S * n_element_col),                            \
-              (M + WARP_SIZE_S - 1) / (WARP_SIZE_S / n_element_row));                           \
+              (M + WARP_SIZE_S - 1) / (WARP_SIZE_S * n_element_row));                           \
     mat_transpose_##tag##2d_kernel <<<grid, block>>>(                                           \
       reinterpret_cast<element_type *>(x.data_ptr()),                                           \
       reinterpret_cast<element_type *>(y.data_ptr()), M, N);                                    \
@@ -325,6 +239,7 @@ TORCH_BINDING_MAT_TRANSPOSE(f32x4_row2col, torch::kFloat32, float, 4)
 TORCH_BINDING_MAT_TRANSPOSE2D(f32_col2row, torch::kFloat32, float, 1, 1)
 TORCH_BINDING_MAT_TRANSPOSE2D(f32_row2col, torch::kFloat32, float, 1, 1)
 TORCH_BINDING_MAT_TRANSPOSE2D(f32x4_col2row, torch::kFloat32, float, 1, 4)
+// 2d 索引启动时是按照 / n_element_row 的，是否不太对
 TORCH_BINDING_MAT_TRANSPOSE2D(f32x4_row2col, torch::kFloat32, float, 4, 1)
 // diagonal index method.
 TORCH_BINDING_MAT_TRANSPOSE2D(f32_diagonal, torch::kFloat32, float, 1, 1)
